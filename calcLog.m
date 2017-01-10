c35f1b65249ee0943d9f530c308496331a1bd821
nezabelej <nezabelej@gmail.com>
2017-01-10 15:45:50 +0100

    Algorithm finished

--- calcLog.m
+++ calcLog.m
@@ -1,38 +1,22 @@
-function [ kernel ] = calcLog( sigma )
- 
-    kSize = ceil(4*sigma*sqrt(2));
-   
-    if (rem(kSize, 2) == 0)
-        kSize = kSize+1;
+function [ krnelN ] = calcLoG( sigma )
+  kSize = ceil(sigma*4*sqrt(2));
+  if rem(kSize,2)==0 kSize=kSize+1; end
+  hSize=floor(kSize/2);
+  
+  [X,Y]=meshgrid(-hSize:hSize,-hSize:hSize);
+  %laplacian of a gaussian
+  krnel=((X.^2+Y.^2-2*(sigma^2))./(sigma^4)).*exp(-(X.^2+Y.^2)./(2*sigma^2));
+
+  % calculate kernel with integer coefficients (sum of matrix is 0)
+  krnel = krnel-(mean(krnel(:)));
+  mSuma=sum(round(krnel(:).*(-1.49/krnel(hSize+1,1))));
+  valS=-1.49;
+  for v=-1.48:0.01:-0.50
+    suma=sum(round(krnel(:).*(v/krnel(hSize+1,1))));
+    if ((abs(mSuma)>abs(suma)))
+      mSuma=suma;
+      valS=v;
     end
-    hSize = floor(kSize/2);
-    
- 
- 
-   
-    [X, Y] = meshgrid(-hSize:hSize, -hSize:hSize);
-   
-    kernel = ((X.^2 + Y.^2 - 2*sigma^2)./(sigma^4)).* exp(-(X.^2 + Y.^2)/(2*sigma^2))/(2*pi*sigma^2);
-   
-    kernel = kernel-(mean(kernel(:)));
-   
-    mSuma = sum(round(kernel(:).*(-1.49/kernel(hSize + 1, 1))));
-   
-    valS = -1.49;
-   
-    for v=-1.49:0.01:-0.50
-       
-        suma = sum(round(kernel(:).*(v/kernel(hSize+1, 1))));
-       
-        if (abs(mSuma) > abs(suma))
-            mSuma = suma;
-            valS = v;
-           
-        end    
-           
-       
-    end
-   
-    kernel = round(kernel.*(valS/kernel(hSize, 1)));
- 
+  end
+  krnelN=round(krnel.*(valS/krnel(hSize+1,1)));
 end
