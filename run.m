07dfec20f508e9e5df31a17e8d3e103dc0f925b0
nezabelej <nezabelej@gmail.com>
2017-01-10 16:00:42 +0100

    Report added

--- run.m
+++ run.m
@@ -1,7 +1,13 @@
 I = imread('image1.png');
+
 sigma = min(size(I))*0.005; % sigma=0.5% of min. image size dimension in pixels.
+
+%Calculate coefficients of the LoG. Result is the mask.
 M = calcLog(sigma); % Calculation of the mask of the LoG filter.
+
+%Applying mask on the image.
 F = filter2(M,I); % Filters the data in I with the two-dimensional FIR filter in the matrix M. Result is the same size as I.
+
 figure(1); clf;
 imshow(I);
 colormap gray;
@@ -11,4 +17,5 @@ colormap gray;
 final = marrHildreth(F,0); % Finds the zero crossings of F to determine the locations of edges in I
 figure(3); clf;
 imagesc(final);
+imwrite(final, 'output.png');
 colormap gray;
